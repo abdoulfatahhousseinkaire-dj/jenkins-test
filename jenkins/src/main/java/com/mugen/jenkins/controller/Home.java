632dd3d4611f9bb38d9b1aefb3a2a477a0755ab6
abdoulfatahhousseinkaire-dj <abdoulfatahhousseinkaire@gmail.com>
2026-06-12 17:26:32 +0300

    Ajout d'un message via un controlleur

---
+++ jenkins/src/main/java/com/mugen/jenkins/controller/Home.java
@@ -0,0 +1,14 @@
+package com.mugen.jenkins.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Home {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Bienvenue dans ton app avec le cicd jenkins";
+    }
+}
