1f6ca001629614933f8bf08c24af2c605389f07b
abdoulfatahhousseinkaire-dj <abdoulfatahhousseinkaire@gmail.com>
2026-06-13 23:06:19 +0300

    Ajout des logs

--- jenkins/src/main/java/com/mugen/jenkins/controller/Home.java
+++ jenkins/src/main/java/com/mugen/jenkins/controller/Home.java
@@ -1,14 +1,23 @@
 package com.mugen.jenkins.controller;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Home {
 
+    private static final Logger log = LoggerFactory.getLogger(Home.class);
+
+
     @GetMapping("/")
     public String hello() {
-        return "Bienvenue dans ton app avec le cicd jenkins :) pipeline fonctionnel";
+        log.info("Utilisateur connecté : mugen");
+        log.info("Type de connexion : ssh");
+        log.info("Appareil : Lenovo");
+        log.info("OS : windows");
+        return "Bienvenue dans ton app avec le cicd jenkins :) pipeline ok";
     }
 }
