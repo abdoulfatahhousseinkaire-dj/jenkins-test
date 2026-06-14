e1a45a332094c6fbe230823cf60e1f1f882fb5aa
abdoulfatahhousseinkaire-dj <abdoulfatahhousseinkaire@gmail.com>
2026-06-14 10:42:33 +0300

    Add RestController in TechStack Controller

--- jenkins/src/main/java/com/mugen/jenkins/controller/TechStack.java
+++ jenkins/src/main/java/com/mugen/jenkins/controller/TechStack.java
@@ -3,7 +3,9 @@ package com.mugen.jenkins.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class TechStack {
 
     private static final Logger log = LoggerFactory.getLogger(TechStack.class);
